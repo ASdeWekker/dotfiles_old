878a500a29c36370ba8c871a40a96feed6dfea5e
Alex de Wekker <alexdewekker6@gmail.com>
2017-09-23 23:14:06 +0000

    Made an npm install sh for arch arm

--- net-start.sh
+++ net-start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo netctl stop eth0
-sudo ip link set eth0 down
-sudo netctl restart eth0
+sudo netctl stop eth0;
+sudo ip link set eth0 down;
+sudo netctl restart eth0;
---
+++ npminstall-archarm.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd /srv/http/backup/oefenen/node/addpage;
+npm i;
+cd /srv/http/backup/oefenen/node/expresstut;
+npm i;
+cd /srv/http/backup/oefenen/node/meantut;
+npm i;
+cd /srv/http/backup/oefenen/node/nodemongo;
+npm i;
+cd /srv/http/backup/oefenen/node/nsession;
+npm i;
+cd /srv/http/backup/oefenen/node/session;
+npm i;
