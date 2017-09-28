71cc256698b1180298ff67b8ebda10da26ac12f7
Alex de Wekker <alexdewekker6@gmail.com>
2017-09-28 16:03:36 +0000

    Added folders for npminstall-ubunduuu.sh and added an ssh alias

--- npminstall-ubundu.sh
+++ npminstall-ubundu.sh
@@ -12,3 +12,7 @@ cd /var/www/html/backup/oefenen/node/nsession;
 npm i;
 cd /var/www/html/backup/oefenen/node/session;
 npm i;
+cd /var/www/html/backup/projects/urlname;
+npm i;
+cd /var/www/html/backup/projects/darten;
+npm i;
