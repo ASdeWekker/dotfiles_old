7d149a65bf85cee07028e7bdd86d771f25821e8c
Alex de Wekker <alexdewekker6@gmail.com>
2017-10-28 19:00:24 +0200

    batch conversion scripts for drone footage and such

--- batchconv1080p.sh
+++ batchconv1080p.sh
@@ -3,5 +3,5 @@
 for i in *.MP4;
     do name=`echo $i | cut -d'.' -f1`;
     echo $name;
-    ffmpeg -i "$i" -r 24 -vf scale=1920:1080,setdar=16:9 "${name}.MP4";
+    ffmpeg -i "$i" -r 24 -vf scale=1920:1080,setdar=16:9 1080p/"${name}.MP4";
 done
--- batchconv24fps.sh
+++ batchconv24fps.sh
@@ -3,5 +3,5 @@
 for i in *.MP4;
     do name=`echo $i | cut -d'.' -f1`;
     echo $name;
-    ffmpeg -i "$i" -r 24 "${name}.MP4";
+    ffmpeg -i "$i" -r 24 24fps/"${name}.MP4";
 done
