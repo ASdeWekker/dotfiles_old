41f3100bde8b48673a08c0425312aea16e051ad0
Alex de Wekker <alexdewekker6@gmail.com>
2017-11-02 07:13:39 +0000

    added some text

--- archarm/updatearch.sh
+++ archarm/updatearch.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+echo "Updating Arch ARM..."
 sudo pacman -Syu --noconfirm;
+echo "...";
 yaourt -Syu --noconfirm;
--- ubundu/updateubundu.sh
+++ ubundu/updateubundu.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+echo "Updating Ubuntu..."
 sudo apt update;
 sudo apt upgrade -y;
 sudo apt full-upgrade -y;
+echo "...";
 sudo apt autoclean -y;
 sudo apt autoremove -y;
