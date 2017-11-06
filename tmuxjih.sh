ad111af828277544df5e8e1bf103db216089204c
Alex de Wekker <alexdewekker6@gmail.com>
2017-11-06 07:52:53 +0000

    things are happening

--- tmuxjih.sh
+++ tmuxjih.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 tmux new-session -s grassbot -n raampje
-tmux new-session -s hmm -n raampie
-tmux new-session -s hmma -n rampaats
+neww
+#tmux new-session -s hmm -n raampie
+#tmux new-session -s hmma -n rampaats
