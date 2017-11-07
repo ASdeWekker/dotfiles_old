5c290083a1b89aca899ca80a7050f62d3b00ad8f
Alex de Wekker <alexdewekker6@gmail.com>
2017-11-07 07:10:35 +0000

    added a stop exec for most pm2 projects

--- archarm/bootding.sh
+++ archarm/bootding.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+#
+# STARTUP ALL THE STUFF IM USING
+#
+sudo systemctl start pm2-alex
 sudo systemctl start smbd;
 sudo systemctl start httpd;
 sudo systemctl start mongodb;
 sudo systemctl start nginx;
-sudo systemctl start pm2-alex;
 #sudo systemctl start
 #sudo systemctl start
 #sudo systemctl start
+
+# STOP ALMOST ALL THE PM2 STUFF
+pm2 stop 1 2 3 4 5 6 7
