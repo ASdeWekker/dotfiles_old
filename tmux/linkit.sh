1ca51a0a200c99317fd7e8e9465965b0e28c757a
Alex de Wekker <alexdewekker6@gmail.com>
2017-11-08 07:29:02 +0000

    made linkit file, still need to add foreach for easy adding

--- tmux/linkit.sh
+++ tmux/linkit.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+filesloc="/home/alex/shn/tmux"
+linkloc="/home/alex/.config/tmuxinator/"
 
+ln -s $filesloc/alles.yml $linkloc;
+ln -s $filesloc/hmm.yml $linkloc;
+ln -s $filesloc/hmma.yml $linkloc;
+ln -s $filesloc/grassbot.yml $linkloc;
