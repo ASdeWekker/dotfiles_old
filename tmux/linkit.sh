7371764fe5e3a7e215530c826a0c542bc43fe79f
Alex de Wekker <alexdewekker6@gmail.com>
2017-11-09 07:24:01 +0000

    fixed to make it automatic for easy reinstall

--- tmux/linkit.sh
+++ tmux/linkit.sh
@@ -3,7 +3,12 @@
 filesloc="/home/alex/shn/tmux"
 linkloc="/home/alex/.config/tmuxinator/"
 
-ln -s $filesloc/alles.yml $linkloc;
-ln -s $filesloc/hmm.yml $linkloc;
-ln -s $filesloc/hmma.yml $linkloc;
-ln -s $filesloc/grassbot.yml $linkloc;
+#ln -s $filesloc/alles.yml $linkloc;
+#ln -s $filesloc/hmm.yml $linkloc;
+#ln -s $filesloc/hmma.yml $linkloc;
+#ln -s $filesloc/grassbot.yml $linkloc;
+
+for i in *.yml;
+    do echo $i;
+    ln -s $filesloc/$i $linkloc;
+done
