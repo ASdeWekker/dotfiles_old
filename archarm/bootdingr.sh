39bf3ecd7743f052a06b4304202f8a8d4d8a0968
Alex de Wekker <alexdewekker6@gmail.com>
2017-11-11 17:06:52 +0000

    changed pm2-alex startup to the back of the file

--- archarm/bootdingr.sh
+++ archarm/bootdingr.sh
@@ -3,11 +3,11 @@
 #
 # STARTUP ALL THE STUFF IM USING
 #
-systemctl start pm2-alex;
 systemctl start smbd;
 systemctl start httpd;
 systemctl start mongodb;
 systemctl start nginx;
+systemctl start pm2-alex;
 #systemctl start
 #systemctl start
 #systemctl start
