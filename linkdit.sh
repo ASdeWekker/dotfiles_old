cc54f7f15f2b4c87f115e0aa95e8076bcebaf04f
Alex de Wekker <alexdewekker6@gmail.com>
2017-11-11 15:11:16 +0000

    added a linking file for a fresh install

---
+++ linkdit.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo ln -s dag.sh /usr/local/bin/doei
+sudo ln -s gedag.sh /usr/local/bin/doeii
+sudo ln -s batchconv1080p.sh /usr/local/bin/conv1080
+sudo ln -s batchconv24fps.sh /usr/local/bin/conv24
