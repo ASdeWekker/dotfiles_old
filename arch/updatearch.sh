9db8979eeb1b3bdd5daab78c1b8f6f9ca12187d7
Alex de Wekker <alexdewekker6@gmail.com>
2018-02-16 12:19:30 +0100

    changed it all back to pacaur and yaourt

--- arch/updatearch.sh
+++ arch/updatearch.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 echo "Updating Arch..."
-yay -Syu --noconfirm;
-#sudo pacman -Syu --noconfirm;
-#echo "...";
-#yaourt -Syu --noconfirm;
+sudo pacman -Syu --noconfirm;
+echo "...";
+yaourt -Syu --noconfirm;
