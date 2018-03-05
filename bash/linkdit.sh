a3a7b1954d6d9b07c1bb8528cf80107d343ab1b1
Alex de Wekker <alexdewekker6@gmail.com>
2018-03-05 21:33:38 +0100

    added suspends.sh to linkdit.sh

--- bash/linkdit.sh
+++ bash/linkdit.sh
@@ -6,3 +6,4 @@ sudo ln -s $shn/dag.sh $ulb/doei
 sudo ln -s $shn/gedag.sh $ulb/doeii
 sudo ln -s $shn/batchconv1080p.sh $ulb/conv1080
 sudo ln -s $shn/batchconv24fps.sh $ulb/conv24
+sudo ln -s $shn/suspends.sh $ulb/slp
