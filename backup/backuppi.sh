7bd27fc96e2ee64606aa97a8cb89d4752fd46e5c
Alex de Wekker <alexdewekker6@gmail.com>
2018-04-13 13:26:05 +0200

    changed script, is now ready for live test

--- backup/backuppi.sh
+++ backup/backuppi.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-#ssh root@taaart dd if=/dev/mmcblk0 bs=1M | pv | gzip -c > /mnt/data/Backup/taaart/$(date -I)-taaart.gz
-touch /mnt/data/Backup/taaart/$(date -I)-taaart.gz
+ssh root@taaart dd if=/dev/mmcblk0 bs=1M | pv | gzip -c > /mnt/data/Backup/taaart/$(date -I)-taaart.gz
+#touch /mnt/data/Backup/taaart/$(date -I)-taaart.gz
+sleep 1
 systemctl poweroff
