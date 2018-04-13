164bc3bfbadd6474c9925e3246a3b2029569bb8f
Alex de Wekker <alexdewekker6@gmail.com>
2018-04-13 12:26:31 +0200

    changed back to poweroff and letting script run by root

--- backup/backuppi.sh
+++ backup/backuppi.sh
@@ -2,4 +2,4 @@
 
 #ssh root@taaart dd if=/dev/mmcblk0 bs=1M | pv | gzip -c > /mnt/data/Backup/taaart/$(date -I)-taaart.gz
 touch /mnt/data/Backup/taaart/$(date -I)-taaart.gz
-slp
+systemctl poweroff
