eea30831157f3e5c63facda47165d1e3a3c1661d
Alex de Wekker <alexdewekker6@gmail.com>
2018-04-15 14:50:34 +0200

    removed pv

--- backup/backuppi.sh
+++ backup/backuppi.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-ssh root@taaart dd if=/dev/mmcblk0 bs=1M | pv | gzip -c > /mnt/data/Backup/taaart/$(date -I)-taaart.gz && systemctl poweroff
+ssh root@taaart dd if=/dev/mmcblk0 bs=1M | gzip -c > /mnt/data/Backup/taaart/$(date -I)-taaart.gz && systemctl poweroff
