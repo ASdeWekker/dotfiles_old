76a3a8d70debb570e7a86f98cecdbb04b09e1d72
Alex de Wekker <alexdewekker6@gmail.com>
2018-04-19 07:15:24 +0200

    added some comments

--- backup/commands.sh
+++ backup/commands.sh
@@ -1,17 +1,19 @@
-use a command to backup the root directory once per 3 nights
-use a command to backup the home folder every night
-tar and gzip the resulting stuff in the that should be
-outputted to the temp folder in the Backup folder
-make a structure where it will eventually hold a backup
-of every month.
-figure out how to connect with ssh to the pi
-root pi command:
+#!/bin/bash
+
+# use a command to backup the root directory once per 3 nights
+# use a command to backup the home folder every night
+# tar and gzip the resulting stuff in the that should be
+# outputted to the temp folder in the Backup folder
+# make a structure where it will eventually hold a backup
+# of every month.
+# figure out how to connect with ssh to the pi - figured it out!
+# root pi command:
 sudo rsync -aAXvq -e ssh --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","/home/*"} taartr:/ /mnt/data/Backup/temp/pi-root/
-home pi command:
+# home pi command:
 sudo rsync -aAXvq -e ssh --exclude={"/home/"} taartr:/home/alex/ /mnt/data/Backup/temp/pi-home/
-still need to figure out where exactly to store it
-commands for backing up the pc
-root pc command:
+# still need to figure out where exactly to store it
+# commands for backing up the pc
+# root pc command:
 sudo rsync -aAXvq --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","/home/*"} / /mnt/data/Backup/temp/pc-root/
-home pc command:
+# home pc command:
 sudo rsync -aAXvq --exclude={"/home/.cache/*???"} /home/alex /mnt/data/Backup/temp/pc-home/
