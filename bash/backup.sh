0bde6bee33cd8e104d14428ae96d99f5aaffc0c8
Alex de Wekker <alexdewekker6@gmail.com>
2018-04-22 16:29:27 +0200

    removed verbose option from tar

--- bash/backup.sh
+++ bash/backup.sh
@@ -25,7 +25,7 @@ gzloc="/mnt/data/backup/gzip"
 rsync -aAXvq -e ssh --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","/home/*","/var/www/html/backup/*"} taartr:/ $bloc/rsync/pi-root/
 
 # tar and gzip
-env GZIP=-9 tar cvzf $gzloc/pi/$(date -I)-pi-root.tar.gz $bloc/rsync/pi-root/*
+env GZIP=-9 tar czf $gzloc/pi/$(date -I)-pi-root.tar.gz $bloc/rsync/pi-root/*
 
 # sleep for a bit
 sleep 2
@@ -34,7 +34,7 @@ sleep 2
 rsync -aAXvq -e ssh --exclude={"/home/*/.thumbnails/*","/home/*/.cache/mozilla/*","/home/*/.cache/chromium/*","/home/*/.local/share/Trash/*","/home/*/.gvfs","/home/*/shn/*"} taartr:/home/alex/ $bloc/rsync/pi-home/
 
 # tar and gzip
-env GZIP=-9 tar cvzf $gzloc/pi/$(date -I)-pi-home.tar.gz $bloc/rsync/pi-home/*
+env GZIP=-9 tar czf $gzloc/pi/$(date -I)-pi-home.tar.gz $bloc/rsync/pi-home/*
 
 # sleep for a bit
 sleep 2
@@ -43,7 +43,7 @@ sleep 2
 rsync -aAXvq --exclude={"/home/*/.thumbnails/*","/home/*/.cache/mozilla/*","/home/*/.cache/chromium/*","/home/*/.local/share/Trash/*","/home/*/.gvfs","/home/*/shn/*"} /home/alex/ $bloc/rsync/pc-home/
 
 # tar and gzip
-env GZIP=-9 tar cvzf $gzloc/pc/$(date -I)-pc-home.tar.gz $bloc/rsync/pc-home/*
+env GZIP=-9 tar czf $gzloc/pc/$(date -I)-pc-home.tar.gz $bloc/rsync/pc-home/*
 
 # sleep for a bit
 sleep 2
@@ -52,7 +52,7 @@ sleep 2
 rsync -aAXvq --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","/home/*","/var/www/html/backup/*"} / $bloc/rsync/pc-root/
 
 # tar and gzip
-env GZIP=-9 tar cvzf $gzloc/pc/$(date -I)-pc-root.tar.gz $bloc/rsync/pc-root/*
+env GZIP=-9 tar czf $gzloc/pc/$(date -I)-pc-root.tar.gz $bloc/rsync/pc-root/*
 
 # sleep for a bit
 sleep 2
