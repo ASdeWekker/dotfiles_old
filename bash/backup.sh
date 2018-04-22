ada4c4f88ed93b7275f9dbb7d31553bc5859adbb
Alex de Wekker <alexdewekker6@gmail.com>
2018-04-22 16:25:59 +0200

    changed directories and such

--- bash/backup.sh
+++ bash/backup.sh
@@ -3,7 +3,7 @@
 # use a command to backup the root directory once per 3 nights
 # use a command to backup the home folder every night
 # tar and gzip the resulting stuff in the that should be
-# outputted to the temp folder in the Backup folder
+# outputted to the rsync folder in the Backup folder
 # make a structure where it will eventually hold a backup
 # of every month.
 # figure out how to connect with ssh to the pi - figured it out!
@@ -17,40 +17,42 @@
 
 # location of the backup folder:
 bloc="/mnt/backup"
+# location were the tar.gz's are stored
+gzloc="/mnt/data/backup/gzip"
 
 # root pi commands:
 # backup
-rsync -aAXvq -e ssh --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","/home/*"} taartr:/ $bloc/temp/pi-root/
+rsync -aAXvq -e ssh --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","/home/*"} taartr:/ $bloc/rsync/pi-root/
 
 # tar and gzip
-env GZIP=-9 tar cvzf $bloc/pi/$(date -I)-pi-root.tar.gz $bloc/temp/pi-root/*
+env GZIP=-9 tar cvzf $gzloc/pi/$(date -I)-pi-root.tar.gz $bloc/rsync/pi-root/*
 
 # sleep for a bit
 sleep 2
 
 # home pi commands:
-rsync -aAXvq -e ssh --exclude={"/home/*/.thumbnails/*","/home/*/.cache/mozilla/*","/home/*/.cache/chromium/*","/home/*/.local/share/Trash/*","/home/*/.gvfs"} taartr:/home/alex/ $bloc/temp/pi-home/
+rsync -aAXvq -e ssh --exclude={"/home/*/.thumbnails/*","/home/*/.cache/mozilla/*","/home/*/.cache/chromium/*","/home/*/.local/share/Trash/*","/home/*/.gvfs"} taartr:/home/alex/ $bloc/rsync/pi-home/
 
 # tar and gzip
-env GZIP=-9 tar cvzf $bloc/pi/$(date -I)-pi-home.tar.gz $bloc/temp/pi-home/*
+env GZIP=-9 tar cvzf $gzloc/pi/$(date -I)-pi-home.tar.gz $bloc/rsync/pi-home/*
 
 # sleep for a bit
 sleep 2
 
 # home pc commands:
-rsync -aAXvq --exclude={"/home/*/.thumbnails/*","/home/*/.cache/mozilla/*","/home/*/.cache/chromium/*","/home/*/.local/share/Trash/*","/home/*/.gvfs"} /home/alex/ $bloc/temp/pc-home/
+rsync -aAXvq --exclude={"/home/*/.thumbnails/*","/home/*/.cache/mozilla/*","/home/*/.cache/chromium/*","/home/*/.local/share/Trash/*","/home/*/.gvfs"} /home/alex/ $bloc/rsync/pc-home/
 
 # tar and gzip
-env GZIP=-9 tar cvzf $bloc/pc/$(date -I)-pc-home.tar.gz $bloc/temp/pc-home/*
+env GZIP=-9 tar cvzf $gzloc/pc/$(date -I)-pc-home.tar.gz $bloc/rsync/pc-home/*
 
 # sleep for a bit
 sleep 2
 
 # root pc commands:
-rsync -aAXvq --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","/home/*"} / $bloc/temp/pc-root/
+rsync -aAXvq --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","/home/*"} / $bloc/rsync/pc-root/
 
 # tar and gzip
-env GZIP=-9 tar cvzf $bloc/pc/$(date -I)-pc-root.tar.gz $bloc/temp/pc-root/*
+env GZIP=-9 tar cvzf $gzloc/pc/$(date -I)-pc-root.tar.gz $bloc/rsync/pc-root/*
 
 # sleep for a bit
 sleep 2
