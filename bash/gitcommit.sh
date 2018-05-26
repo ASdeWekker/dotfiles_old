545e051f4f846c010d9d662b9a258e4497d46597
Alex de Wekker <alexdewekker6@gmail.com>
2018-05-26 18:34:55 +0200

    finalized gitcommit.sh and deleted test files

--- bash/gitcommit.sh
+++ bash/gitcommit.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+commitmessage=$1
+push=$2
+
 git status
 git add -A
-git commit -m $1
-git push
+git commit -m "$commitmessage"
+
+if [ $push == "y" ]; then
+    git push
+    git status
+else
+    git status
+fi
