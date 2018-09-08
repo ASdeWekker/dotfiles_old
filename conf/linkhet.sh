0baa611dc1f5c9be9f248bee29bbe6deee55d4d9
Alex de Wekker <alexdewekker6@gmail.com>
2018-09-09 00:05:56 +0200

    added new dotfiles to the linkfile

--- conf/linkhet.sh
+++ conf/linkhet.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+home="/home/alex"
 homed="/home/alex/shn/conf"
 sudo ln -s $homed/httpd.conf /etc/httpd/conf/
 sudo ln -s $homed/nginx.conf /etc/nginx/
@@ -8,3 +9,7 @@ sudo ln -s $homed/php.ini /etc/php/
 sudo ln -s $homed/httpd/conf/extra/php7_module.conf /etc/httpd/conf/extra/
 sudo ln -s $homed/httpd/conf/extra/php-fpm.conf /etc/httpd/conf/extra/
 sudo ln -s $homed/httpd/conf/extra/phpmyadmin.conf /etc/httpd/conf/extra/
+ln -s $homed/i3/config /home/alex/.config/i3/
+ln -s $homed/.Xresources /home/alex/
+mkdir $home/.bin/
+ln -s $homed/dot-bin/tmuxinator.bash $home/.bin/
