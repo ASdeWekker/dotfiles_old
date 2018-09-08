68b4e61e0a5b18e6c2e74689bbc020e692dc94cc
Alex de Wekker <alexdewekker6@gmail.com>
2018-09-09 00:08:25 +0200

    added the force flag

--- conf/linkhet.sh
+++ conf/linkhet.sh
@@ -2,14 +2,14 @@
 
 home="/home/alex"
 homed="/home/alex/shn/conf"
-sudo ln -s $homed/httpd.conf /etc/httpd/conf/
-sudo ln -s $homed/nginx.conf /etc/nginx/
-sudo ln -s $homed/tmux.conf /etc/
-sudo ln -s $homed/php.ini /etc/php/
-sudo ln -s $homed/httpd/conf/extra/php7_module.conf /etc/httpd/conf/extra/
-sudo ln -s $homed/httpd/conf/extra/php-fpm.conf /etc/httpd/conf/extra/
-sudo ln -s $homed/httpd/conf/extra/phpmyadmin.conf /etc/httpd/conf/extra/
-ln -s $homed/i3/config /home/alex/.config/i3/
-ln -s $homed/.Xresources /home/alex/
+sudo ln -sf $homed/httpd.conf /etc/httpd/conf/
+sudo ln -sf $homed/nginx.conf /etc/nginx/
+sudo ln -sf $homed/tmux.conf /etc/
+sudo ln -sf $homed/php.ini /etc/php/
+sudo ln -sf $homed/httpd/conf/extra/php7_module.conf /etc/httpd/conf/extra/
+sudo ln -sf $homed/httpd/conf/extra/php-fpm.conf /etc/httpd/conf/extra/
+sudo ln -sf $homed/httpd/conf/extra/phpmyadmin.conf /etc/httpd/conf/extra/
+ln -sf $homed/i3/config /home/alex/.config/i3/
+ln -sf $homed/.Xresources /home/alex/
 mkdir $home/.bin/
-ln -s $homed/dot-bin/tmuxinator.bash $home/.bin/
+ln -sf $homed/dot-bin/tmuxinator.bash $home/.bin/
