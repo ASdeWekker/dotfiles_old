ebc9f40fe95d515a74b9f3dbf0f5666995a85325
Alex de Wekker <alexdewekker6@gmail.com>
2018-09-22 22:17:57 +0200

    made some more changes

--- bash/rofi.sh
+++ bash/rofi.sh
@@ -12,4 +12,4 @@ f-f=#00ffff
 g-g=#ffffff
 h-h=#000000
 
-rofi -show run -lines 4 -eh 2 -width 100 -padding 800 -opacity "85" -bw 0 -font "Noto 22" -color-window "$a-a,$b-b,$c-c" -color-normal "$d-d,$e-e,$f-f,$g-g,$h-h"
+rofi -show run -lines 4 -eh 2 -width 100 -padding 800 -opacity "85" -bw 0 -font "Noto 22" #-color-window "$a-a,$b-b,$c-c" -color-normal "$d-d,$e-e,$f-f,$g-g,$h-h"
