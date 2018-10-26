cfa5f204a1b7ad83ce7a31820d9d4eb90dc17a90
Alex de Wekker <alexdewekker6@gmail.com>
2018-10-27 01:11:46 +0200

    removed the doei bash script and added the command to .alias

--- bash/linkdit.sh
+++ bash/linkdit.sh
@@ -2,7 +2,6 @@
 
 ulb="/usr/local/bin"
 shn="/home/alex/shn/bash"
-sudo ln -sf $shn/dag.sh $ulb/doei
 sudo ln -sf $shn/gedag.sh $ulb/doeii
 sudo ln -sf $shn/batchconv1080p.sh $ulb/conv1080
 sudo ln -sf $shn/batchconv24fps.sh $ulb/conv24
