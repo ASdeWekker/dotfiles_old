2d90166b7d7f3acd23e7f32c93418bee137ff9ad
Alex de Wekker <alexdewekker6@gmail.com>
2018-10-27 01:17:53 +0200

    moved the tmux files to this folder

---
+++ conf/5-tmux/linkit.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Location where the files are located
+filesloc="/home/alex/shn/tmux"
+# Location where the files are needed
+linkloc="/home/alex/.config/tmuxinator/"
+
+mkdir -p /home/alex/.config/tmuxinator
+
+# For loop to grab all the .yml files and link them
+for i in *.yml;
+    do echo $i;
+    ln -sf $filesloc/$i $linkloc;
+done
