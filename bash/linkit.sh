9d2e637c20b07dbbe2339c4dd2397914490c8227
Alex de Wekker <alexdewekker6@gmail.com>
2018-11-18 20:20:40 +0100

    now using gitt as a bash function instead of a shell file

--- bash/linkit.sh
+++ bash/linkit.sh
@@ -7,4 +7,3 @@ sudo ln -sf $shn/batchconv24fps.sh $ulb/conv24
 sudo ln -sf $shn/suspends.sh $ulb/slp
 sudo ln -sf $shn/runaswine.sh $ulb/wijn
 sudo ln -sf $shn/mongot.sh $ulb/
-sudo ln -sf $shn/gitcommit.sh $ulb/gitt
