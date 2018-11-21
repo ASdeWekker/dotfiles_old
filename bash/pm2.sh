234c456204954eafad7bb184580167b525b52445
Alex de Wekker <alexdewekker6@gmail.com>
2018-11-21 20:55:08 +0100

    changed the directory to de kelder and commented out some things

--- bash/pm2.sh
+++ bash/pm2.sh
@@ -4,14 +4,14 @@
 bac=/var/www/html/backup
 
 # make a service out of pm2 change the username if necessary.
-sudo env PATH=$PATH:/usr/lib/node_modules/pm2/bin/pm2 startup systemd -u alex --hp /home/alex
+#sudo env PATH=$PATH:/usr/lib/node_modules/pm2/bin/pm2 startup systemd -u alex --hp /home/alex
 
 # enable it.
-sudo systemctl enable pm2-alex
+#sudo systemctl enable pm2-alex
 
 # go to all the folders and start the project.
-cd $bac/nodelpage
-pm2 start nodelpage.js
+cd $bac/../de_kelder/
+pm2 start de_kelder.js
 cd $bac/oefenen/node/expresstut
 pm2 start expresstut.js
 cd $bac/oefenen/node/nodemongo/bin
