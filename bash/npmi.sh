c0b71c6b30bf499f7b436fdf0b0eebceb0740b09
Alex de Wekker <alexdewekker6@gmail.com>
2018-11-24 23:17:57 +0100

    added postgres and de_kelder

--- bash/npmi.sh
+++ bash/npmi.sh
@@ -3,23 +3,25 @@
 
 wpath="/var/www/html/backup"
 
-cd $wpath/oefenen/node/addpage;
-yarn install;
-cd $wpath/oefenen/node/expresstut;
-yarn install;
-cd $wpath/oefenen/node/meantut;
-yarn install;
-cd $wpath/oefenen/node/nodemongo;
-yarn install;
-cd $wpath/oefenen/node/nsession;
-yarn install;
-cd $wpath/oefenen/node/session;
-yarn install;
-cd $wpath/projects/urlname;
-yarn install;
-cd $wpath/projects/darten;
-yarn install;
-cd $wpath/nodelpage;
-yarn install;
-cd $wpath/projects/speedtest;
-yarn install;
+cd $wpath/oefenen/node/addpage
+yarn install
+cd $wpath/oefenen/node/expresstut
+yarn install
+cd $wpath/oefenen/node/meantut
+yarn install
+cd $wpath/oefenen/node/nodemongo
+yarn install
+cd $wpath/oefenen/node/nsession
+yarn install
+cd $wpath/oefenen/node/session
+yarn install
+cd $wpath/projects/urlname
+yarn install
+cd $wpath/projects/darten
+yarn install
+cd $wpath/projects/speedtest
+yarn install
+cd $wpath/../de_kelder
+yarn install
+cd $wpath/oefenen/postgres
+yarn install
