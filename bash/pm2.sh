f609155f39e3be2da9ab5d69dd8bcce96b87bfc3
Alex de Wekker <alexdewekker6@gmail.com>
2018-12-15 13:24:53 +0100

    changed it so the apps gestarted in the project folder instead of the bin folder

--- bash/pm2.sh
+++ bash/pm2.sh
@@ -10,16 +10,16 @@ bac=/var/www/html/backup
 #sudo systemctl enable pm2-alex
 
 # go to all the folders and start the project.
-cd $bac/../de_kelder/2-bin
-pm2 start de_kelder --update-env
+cd $bac/../de_kelder
+pm2 start 2-bin/de_kelder --update-env
 cd $bac/oefenen/node/expresstut
 pm2 start expresstut.js --update-env
-cd $bac/oefenen/node/nodemongo/bin
-pm2 start nodemongo --update-env
+cd $bac/oefenen/node/nodemongo
+pm2 start bin/nodemongo --update-env
 cd $bac/oefenen/node/meantut/bin
 pm2 start meantut --update-env
-cd $bac/oefenen/node/session/bin
-pm2 start session --update-env
+cd $bac/oefenen/node/session
+pm2 start bin/session --update-env
 cd $bac/oefenen/node/addpage
 pm2 start addpage.js --update-env
 cd $bac/oefenen/node/nsession
@@ -30,8 +30,8 @@ cd $bac/projects/urlname
 pm2 start urlname.js --update-env
 cd $bac/projects/speedtest
 pm2 start speedtest.js --update-env
-cd $bac/oefenen/node/postgres/2-bin
-pm2 start postgres --update-env
+cd $bac/oefenen/node/postgres/
+pm2 start 2-bin/postgres --update-env
 pm2 save
 
 # done.
