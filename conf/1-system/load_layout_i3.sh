db9d65d20335889453d3524e82301458bfe418fc
Alex de Wekker <alexdewekker6@gmail.com>
2018-12-31 14:18:37 +0100

    workspace layout almost working

--- conf/1-system/load_layout_i3.sh
+++ conf/1-system/load_layout_i3.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # First we append the saved layout of workspace 3 to workspace 3
-i3-msg "workspace 3; append_layout ~/shn/ws3-dev.json"
+i3-msg "workspace 3 Dev; append_layout ~/shn/ws3-dev.json"
