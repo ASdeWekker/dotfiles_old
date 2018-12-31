4525ecca639f44e493deb303f131c5952233c978
Alex de Wekker <alexdewekker6@gmail.com>
2018-12-31 15:12:39 +0100

    layout saving is not yet working

--- conf/1-system/load_layout_i3.sh
+++ conf/1-system/load_layout_i3.sh
@@ -2,3 +2,8 @@
 
 # First we append the saved layout of workspace 3 to workspace 3
 i3-msg "workspace 3 Dev; append_layout ~/shn/ws3-dev.json"
+
+# Load the needed programs
+(subl3 /var/www/html/de_kelder &)
+(chromium --new-window http://localhost:3099 &)
+(urxvt &)
