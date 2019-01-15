762fbf29a883d5f8e4ce3ec7b98b305935af366d
Alex de Wekker <alexdewekker6@gmail.com>
2019-01-15 20:25:27 +0100

    changed for use with the pm2 ecosystem config file

--- bash/pm2.sh
+++ bash/pm2.sh
@@ -11,25 +11,25 @@ bac=/var/www/html/backup
 
 # go to all the folders and start the project.
 cd $bac/../de_kelder
-pm2 start 2-bin/de_kelder --update-env
+pm2 start --update-env
 cd $bac/oefenen/node/expresstut
-pm2 start expresstut.js --update-env
+pm2 start --update-env
 cd $bac/oefenen/node/nodemongo
-pm2 start bin/nodemongo --update-env
-cd $bac/oefenen/node/meantut/bin
-pm2 start meantut --update-env
+pm2 start --update-env
+cd $bac/oefenen/node/meantut
+pm2 start --update-env
 cd $bac/oefenen/node/session
-pm2 start bin/session --update-env
+pm2 start --update-env
 cd $bac/oefenen/node/addpage
-pm2 start addpage.js --update-env
-cd $bac/oefenen/node/postgres/
-pm2 start 2-bin/postgres --update-env
+pm2 start --update-env
+cd $bac/oefenen/node/postgres
+pm2 start --update-env
 cd $bac/projects/darten
-pm2 start darten.js --update-env
+pm2 start --update-env
 cd $bac/projects/urlname
-pm2 start 2-bin/urlname --update-env
+pm2 start --update-env
 cd $bac/projects/speedtest
-pm2 start 2-bin/speedtest --update-env
+pm2 start --update-env
 pm2 save
 
 # done.
