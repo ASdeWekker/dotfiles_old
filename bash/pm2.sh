24210af3800a27fbf68de98f41d26729de12ac38
Alex de Wekker <alexdewekker6@gmail.com>
2019-01-27 16:29:59 +0100

    switched to using two different pm2 environments

--- bash/pm2.sh
+++ bash/pm2.sh
@@ -11,26 +11,28 @@ bac=/var/www/html/backup
 
 # go to all the folders and start the project.
 cd $bac/../de_kelder
-pm2 start --update-env
+PM2_HOME=$HOME/.pm2p pm2 start --update-env
 cd $bac/oefenen/node/expresstut
-pm2 start --update-env
+PM2_HOME=$HOME/.pm2o pm2 start --update-env
 cd $bac/oefenen/node/nodemongo
-pm2 start --update-env
+PM2_HOME=$HOME/.pm2o pm2 start --update-env
 cd $bac/oefenen/node/meantut
-pm2 start --update-env
+PM2_HOME=$HOME/.pm2o pm2 start --update-env
 cd $bac/oefenen/node/session
-pm2 start --update-env
+PM2_HOME=$HOME/.pm2o pm2 start --update-env
 cd $bac/oefenen/node/addpage
-pm2 start --update-env
+PM2_HOME=$HOME/.pm2o pm2 start --update-env
 cd $bac/oefenen/node/postgres
-pm2 start --update-env
+PM2_HOME=$HOME/.pm2o pm2 start --update-env
 cd $bac/projects/darten
-pm2 start --update-env
+PM2_HOME=$HOME/.pm2p pm2 start --update-env
 cd $bac/projects/urlname
-pm2 start --update-env
+PM2_HOME=$HOME/.pm2p pm2 start --update-env
 cd $bac/projects/speedtest
-pm2 start --update-env
-pm2 save
+PM2_HOME=$HOME/.pm2p pm2 start --update-env
+
+PM2_HOME=$HOME/.pm2o pm2 save
+PM2_HOME=$HOME/.pm2p pm2 save
 
 # done.
 echo "done"
