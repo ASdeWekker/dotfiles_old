08e5db8a115f228a376d6a0bcc54953cd542bc8b
Alex de Wekker <alexdewekker6@gmail.com>
2019-03-26 21:39:01 +0100

    added a service to power the LEDs on

--- conf/desktop.sh
+++ conf/desktop.sh
@@ -48,6 +48,10 @@ mkdir -p $home/.config/GIMP/2.10
 ln -sf $homed/2-misc/gimp/gtkrc $home/.config/GIMP/2.10/
 ln -sf $homed/2-misc/gimp/themerc $home/.config/GIMP/2.10/
 
+# LEDs
+sudo cp $homed/../bash/services/ledon.service /etc/systemd/system/
+sudo systemctl enable ledon
+
 ### 4 - VFIO ###
 
 # EOF #
