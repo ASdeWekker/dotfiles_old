5139f94bb257f4d5b50afd85d64fb92de7ea24f6
Alex de Wekker <alexdewekker6@gmail.com>
2019-04-15 08:31:42 +0200

    made some changes but this file is not needed anymore

--- bash/rofi.sh
+++ bash/rofi.sh
@@ -12,4 +12,6 @@ f-f=#00ffff
 g-g=#ffffff
 h-h=#000000
 
-rofi -show run -lines 4 -eh 2 -width 100 -padding 800 -opacity "85" -bw 0 -font "Noto 22" #-color-window "$a-a,$b-b,$c-c" -color-normal "$d-d,$e-e,$f-f,$g-g,$h-h"
+#rofi -show run -lines 4 -eh 2 -width 100 -padding 800 -opacity "85" -bw 0 -font "Noto 22"
+rofi -show run -width 2560 -fg "#B0E670" -bg "#14161F" -hlfg "#14161F" -hlbg "#B0E670" -bc "#14161F" -location 0 -lines 10 -bw 0 -font "Source Code Pro Bold 22" -padding 770 -separator-style none -opacity 80
+#-color-window "$a-a,$b-b,$c-c" -color-normal "$d-d,$e-e,$f-f,$g-g,$h-h"
