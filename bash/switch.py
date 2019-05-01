45ebf36a1b6ec515bcb3f70cab627af9be30d453
Alex de Wekker <alexdewekker6@gmail.com>
2019-05-01 23:41:38 +0200

    switch is now ready for future switches

--- bash/switch.py
+++ bash/switch.py
@@ -1,13 +1,31 @@
 #!/bin/python
 
-import sys
+import argparse
 import subprocess
 
-params = { "power":str(sys.argv[1]) }
+parser = argparse.ArgumentParser(description="Turn a switch on or off.")
+
+parser.add_argument("-A", "--all", help="All of them")
+parser.add_argument("-o", "--overhead", help="Overhead lamp")
+parser.add_argument("-s", "--standing", help="Standing lamp")
+parser.add_argument("-a", "--amp", help="Amplifier")
+
+args = parser.parse_args()
 
 
-def switch(ip, state):
-	subprocess.call(["curl", "-sS", "http://192.168.1." + str(ip) + "/?power=" + state])
+def request(ip,power):
+	subprocess.call(["curl", "-sS", "http://192.168.1." + str(ip) + "/?power=" + str(power)])
 
 
-switch(223,params["power"])
+if args.all:
+	request(221,args.all)
+	request(222,args.all)
+	request(223,args.all)
+else:
+	if args.overhead:
+		request(221,args.overhead)
+	if args.standing:
+		request(222,args.standing)
+	if args.amp:
+		request(223,args.amp)
+
