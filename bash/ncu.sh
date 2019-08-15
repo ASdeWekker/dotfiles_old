469050276a39ba38e46e022e36422b356a603079
Alex de Wekker <alexdewekker6@gmail.com>
2019-08-15 22:27:56 +0200

    not going to use this anymore

--- bash/ncu.sh
+++ bash/ncu.sh
@@ -1,41 +1,41 @@
 #!/bin/bash
 
-wpath="/var/www/html/backup"
+#wpath="/var/www/html/backup"
 
-cd $wpath/oefenen/node/addpage
-ncu -a
-npm update
-cd $wpath/oefenen/node/expresstut
-ncu -a
-npm update
-cd $wpath/oefenen/node/meantut
-ncu -a
-npm update
-cd $wpath/oefenen/node/nodemongo
-ncu -a
-npm update
-cd $wpath/oefenen/node/nsession
-ncu -a
-npm update
-cd $wpath/oefenen/node/session
-ncu -a
-npm update
-cd $wpath/projects/darten
-ncu -a
-npm update
-cd $wpath/projects/urlname
-ncu -a
-npm update
-cd $wpath/../de_kelder
-ncu -a
-npm update
-cd $wpath/projects/speedtest
-ncu -a
-npm update
-cd $wpath/oefenen/node/postgres
-ncu -a
-npm update
-cd $wpath/projects/weight
-ncu -a
-npm update
-
+#cd $wpath/oefenen/node/addpage
+#ncu -a
+#npm update
+#cd $wpath/oefenen/node/expresstut
+#ncu -a
+#npm update
+#cd $wpath/oefenen/node/meantut
+#ncu -a
+#npm update
+#cd $wpath/oefenen/node/nodemongo
+#ncu -a
+#npm update
+#cd $wpath/oefenen/node/nsession
+#ncu -a
+#npm update
+#cd $wpath/oefenen/node/session
+#ncu -a
+#npm update
+#cd $wpath/projects/darten
+#ncu -a
+#npm update
+#cd $wpath/projects/urlname
+#ncu -a
+#npm update
+#cd $wpath/../de_kelder
+#ncu -a
+#npm update
+#cd $wpath/projects/speedtest
+#ncu -a
+#npm update
+#cd $wpath/oefenen/node/postgres
+#ncu -a
+#npm update
+#cd $wpath/projects/weight
+#ncu -a
+#npm update
+#
