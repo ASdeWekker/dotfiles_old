54b716c32bb198707fdcaab372eb2a80177e8df5
Alex de Wekker <alexdewekker6@gmail.com>
2019-08-18 16:09:25 +0200

    not needed anymore

--- scripts/bash/login.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo cp -f /var/lib/lightdm/.Xauthority /home/alex/
-sudo chown alex:wheel /home/alex/.Xauthority
-export AUTHORITY=/home/alex/.Xauthority
-export DISPLAY=:0
--- scripts/bash/ws3-dev.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-subl3 /var/www/html/de_kelder
-chromium --new-window http://localhost:3099
-urxvt
