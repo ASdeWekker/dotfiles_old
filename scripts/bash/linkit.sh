bde0c955ed82dc168e7bfc99a4e066522d0efa92
Alex de Wekker <alexdewekker6@gmail.com>
2019-08-18 16:07:01 +0200

    renamed bash to scripts and made more subfolders

--- scripts/bash/linkit.sh
+++ scripts/bash/linkit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ulb="/usr/local/bin"
-shn="/home/alex/shn/bash"
+shn="/home/alex/shn/scripts/bash"
 sudo ln -sf $shn/batchconv1080p.sh $ulb/conv1080
 sudo ln -sf $shn/batchconv24fps.sh $ulb/conv24
 sudo ln -sf $shn/runaswine.sh $ulb/wijn
