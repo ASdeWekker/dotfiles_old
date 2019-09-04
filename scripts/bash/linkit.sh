641faba81bc81a5aaaaaa145f39e5ae80ff418db
Alex de Wekker <alexdewekker6@gmail.com>
2019-09-04 21:07:02 +0200

    changing to the new radarr and sonarr

--- scripts/bash/linkit.sh
+++ scripts/bash/linkit.sh
@@ -6,3 +6,5 @@ sudo ln -sf $shn/batchconv1080p.sh $ulb/conv1080
 sudo ln -sf $shn/batchconv24fps.sh $ulb/conv24
 sudo ln -sf $shn/runaswine.sh $ulb/wijn
 sudo ln -sf $shn/create-pug.sh $ulb/create-pug
+sudo ln -sf $shn/../python/switch.py $ulb/sw
+sudo ln -sf $shn/../python/ledstrip.py $ulb/led
