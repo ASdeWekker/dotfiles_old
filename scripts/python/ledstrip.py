d815c67e9b18716bb891c6fa3caa39b58cf0585e
Alex de Wekker <alexdewekker6@gmail.com>
2019-09-19 21:48:56 +0200

    removed old code

--- scripts/python/ledstrip.py
+++ scripts/python/ledstrip.py
@@ -2,7 +2,6 @@
 
 import argparse
 import requests
-#import subprocess
 
 
 # Argparse configuration
@@ -10,14 +9,7 @@ parser = argparse.ArgumentParser(description="control this ledstrip")
 parser.add_argument("-p", "--power", help="to turn on or off")
 args = parser.parse_args()
 
-
-# A function for curling the argument.
-#def request(power):
-	#subprocess.call(["curl", "-d", "power=" + str(power), "http://192.168.1.220/power"])
-
-
 # Check if the power argument was passed and execute the function.
 if args.power:
-	#request(args.power)
 	data = {"power": str(args.power)}
 	response = requests.post("http://192.168.1.220/power", data=data)
