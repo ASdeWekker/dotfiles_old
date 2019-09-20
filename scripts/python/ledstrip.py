72213343d2f44b91072b582676d78237128cdf80
Alex de Wekker <alexdewekker6@gmail.com>
2019-09-20 13:22:17 +0200

    added a new request for the ledstrip, further testing at home

--- scripts/python/ledstrip.py
+++ scripts/python/ledstrip.py
@@ -7,9 +7,13 @@ import requests
 # Argparse configuration.
 parser = argparse.ArgumentParser(description="control this ledstrip")
 parser.add_argument("-p", "--power", help="to turn on or off")
+parser.add_argument("-c", "--color", help="choose a color")
 args = parser.parse_args()
 
 # Check if the power argument was passed and execute the function.
 if args.power:
 	#data = {"power": str(args.power)}
-	response = requests.post("http://192.168.1.220/power", data={"power": str(args.power)})
+	response = requests.post("http://192.168.1.220/", data={"power": str(args.power)})
+
+if args.color:
+	response = requests.post("http://192.168.1.220/", data={"color": str(arg.color)})
