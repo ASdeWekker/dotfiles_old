b22557e9659eafff26ae0d1e45a7d46a44f496bc
Alex de Wekker <alexdewekker6@gmail.com>
2019-09-20 20:14:31 +0200

    switched back to different endpoints per function

--- scripts/python/ledstrip.py
+++ scripts/python/ledstrip.py
@@ -13,7 +13,7 @@ args = parser.parse_args()
 # Check if the power argument was passed and execute the function.
 if args.power:
 	#data = {"power": str(args.power)}
-	response = requests.post("http://192.168.1.220/", data={"power": str(args.power)})
+	response = requests.post("http://192.168.1.220/power", data={"power": str(args.power)})
 
 if args.color:
-	response = requests.post("http://192.168.1.220/", data={"color": str(arg.color)})
+	response = requests.post("http://192.168.1.220/color", data={"color": str(args.color)})
