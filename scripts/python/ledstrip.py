658d9f17f54566c514fe1889122c3c3cda4304b9
Alex de Wekker <alexdewekker6@gmail.com>
2019-09-20 13:07:12 +0200

    made it into one line of code

--- scripts/python/ledstrip.py
+++ scripts/python/ledstrip.py
@@ -11,5 +11,5 @@ args = parser.parse_args()
 
 # Check if the power argument was passed and execute the function.
 if args.power:
-	data = {"power": str(args.power)}
-	response = requests.post("http://192.168.1.220/power", data=data)
+	#data = {"power": str(args.power)}
+	response = requests.post("http://192.168.1.220/power", data={"power": str(args.power)})
