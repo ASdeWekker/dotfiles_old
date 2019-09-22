2c355d567f18e557b106b533d7dfeaa1fa82b74a
Alex de Wekker <alexdewekker6@gmail.com>
2019-09-22 20:37:21 +0200

    added a couple of new endpoints

--- scripts/python/ledstrip.py
+++ scripts/python/ledstrip.py
@@ -8,10 +8,13 @@ import requests
 ip = "http://192.168.1.220/"
 
 # Argparse configuration.
-parser = argparse.ArgumentParser(description="control this ledstrip")
-parser.add_argument("-p", "--power", help="to turn on or off")
-parser.add_argument("-c", "--color", help="choose a color")
-parser.add_argument("-r", "--rgb", help="choose a color via rgb value")
+parser = argparse.ArgumentParser(description="control this ledstrip.")
+parser.add_argument("-p", "--power", help="to turn on or off.")
+parser.add_argument("-c", "--color", help="choose a color.")
+parser.add_argument("-r", "--rgb", help="choose a color via rgb value.")
+parser.add_argument("-b", "--brightness", help="choose the brightness")
+parser.add_argument("-h", "--hsv", help="choose a color via hsv value")
+parser.add_argument("-R", "--rainbow", help="pick the rainbow function and choose a speed at which the color changes")
 args = parser.parse_args()
 
 # Check if the power argument was passed and execute the function.
@@ -24,3 +27,12 @@ if args.color:
 
 if args.rgb:
 	response = requests.post(ip + "rgb", data={"rgb": str(args.rgb)})
+
+if args.hsv:
+	response = requests.post(ip + "hsv", data={"hsv": str(args.hsv)})
+
+if args.brightness:
+	response = requests.post(ip + "brightness", data={"brightness": str(args.brightness)})
+
+if args.rainbow:
+	response = requests.post(ip + "rainbow", data={"speed": str(args.rainbow)})
