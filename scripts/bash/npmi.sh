3637d0a944c7e59164fad23877ee1ca57c3a738f
Alex de Wekker <alexdewekker6@gmail.com>
2019-09-26 23:02:23 +0200

    added the new kelder location and removed nsession

--- scripts/bash/npmi.sh
+++ scripts/bash/npmi.sh
@@ -10,8 +10,6 @@ cd $wpath/practice/javascript/node/meantut
 yarn install
 cd $wpath/practice/javascript/node/nodemongo
 yarn install
-cd $wpath/practice/javascript/node/nsession
-yarn install
 cd $wpath/practice/javascript/node/session
 yarn install
 cd $wpath/projects/urlname
@@ -20,7 +18,7 @@ cd $wpath/projects/darten
 yarn install
 cd $wpath/projects/speedtest
 yarn install
-cd $wpath/../de_kelder
+cd $wpath/../kelder
 yarn install
 cd $wpath/practice/javascript/node/postgres
 yarn install
