dc822d704543cf88586d9cee41159bff14ee13cb
Alex de Wekker <alexdewekker6@gmail.com>
2019-09-26 23:02:23 +0200

    not needed anymore

--- scripts/bash/pm2.sh
+++ scripts/bash/pm2.sh
@@ -4,31 +4,31 @@
 bac=/var/www/html/penp
 
 # go to all the folders and start the project.
-cd $bac/../de_kelder
-PM2_HOME=$HOME/.pm2p pm2 start --update-env
-cd $bac/practice/javascript/node/expresstut
-PM2_HOME=$HOME/.pm2o pm2 start --update-env
-cd $bac/practice/javascript/node/nodemongo
-PM2_HOME=$HOME/.pm2o pm2 start --update-env
-cd $bac/practice/javascript/node/meantut
-PM2_HOME=$HOME/.pm2o pm2 start --update-env
-cd $bac/practice/javascript/node/session
-PM2_HOME=$HOME/.pm2o pm2 start --update-env
-cd $bac/practice/javascript/node/addpage
-PM2_HOME=$HOME/.pm2o pm2 start --update-env
-cd $bac/practice/javascript/node/postgres
-PM2_HOME=$HOME/.pm2o pm2 start --update-env
-cd $bac/projects/darten
-PM2_HOME=$HOME/.pm2p pm2 start --update-env
-cd $bac/projects/urlname
-PM2_HOME=$HOME/.pm2p pm2 start --update-env
-cd $bac/projects/speedtest
-PM2_HOME=$HOME/.pm2p pm2 start --update-env
-cd $bac/projects/weight
-PM2_HOME=$HOME/.pm2p pm2 start --update-env
+# cd $bac/../de_kelder
+# PM2_HOME=$HOME/.pm2p pm2 start --update-env
+# cd $bac/practice/javascript/node/expresstut
+# PM2_HOME=$HOME/.pm2o pm2 start --update-env
+# cd $bac/practice/javascript/node/nodemongo
+# PM2_HOME=$HOME/.pm2o pm2 start --update-env
+# cd $bac/practice/javascript/node/meantut
+# PM2_HOME=$HOME/.pm2o pm2 start --update-env
+# cd $bac/practice/javascript/node/session
+# PM2_HOME=$HOME/.pm2o pm2 start --update-env
+# cd $bac/practice/javascript/node/addpage
+# PM2_HOME=$HOME/.pm2o pm2 start --update-env
+# cd $bac/practice/javascript/node/postgres
+# PM2_HOME=$HOME/.pm2o pm2 start --update-env
+# cd $bac/projects/darten
+# PM2_HOME=$HOME/.pm2p pm2 start --update-env
+# cd $bac/projects/urlname
+# PM2_HOME=$HOME/.pm2p pm2 start --update-env
+# cd $bac/projects/speedtest
+# PM2_HOME=$HOME/.pm2p pm2 start --update-env
+# cd $bac/projects/weight
+# PM2_HOME=$HOME/.pm2p pm2 start --update-env
 
-PM2_HOME=$HOME/.pm2o pm2 save
-PM2_HOME=$HOME/.pm2p pm2 save
+# PM2_HOME=$HOME/.pm2o pm2 save
+# PM2_HOME=$HOME/.pm2p pm2 save
 
 # done.
 echo "done"
