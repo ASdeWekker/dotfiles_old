f8a75714684b96f7deba3def68ef9b2de7407e61
Alex de Wekker <alexdewekker6@gmail.com>
2019-09-28 09:07:49 +0200

    forgot to switch this part to dotfiles

--- conf/4-vfio/vfioinput/input_attach.sh
+++ conf/4-vfio/vfioinput/input_attach.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-sudo virsh attach-device win10 /home/alex/shn/conf/4-vfio/vfioinput/input_mouse.xml --current
-sudo virsh attach-device win10 /home/alex/shn/conf/4-vfio/vfioinput/input_keyboard.xml --current
+sudo virsh attach-device win10 /home/alex/dotfiles/conf/4-vfio/vfioinput/input_mouse.xml --current
+sudo virsh attach-device win10 /home/alex/dotfiles/conf/4-vfio/vfioinput/input_keyboard.xml --current
--- conf/4-vfio/vfioinput/input_detach.sh
+++ conf/4-vfio/vfioinput/input_detach.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-sudo virsh detach-device win10 /home/alex/shn/conf/4-vfio/vfioinput/input_mouse.xml
-sudo virsh detach-device win10 /home/alex/shn/conf/4-vfio/vfioinput/input_keyboard.xml
+sudo virsh detach-device win10 /home/alex/dotfiles/conf/4-vfio/vfioinput/input_mouse.xml
+sudo virsh detach-device win10 /home/alex/dotfiles/conf/4-vfio/vfioinput/input_keyboard.xml
