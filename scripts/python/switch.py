42a551d0020b9c79adcd152d318eea50829d39e9
Alex de Wekker <alexdewekker6@gmail.com>
2019-10-09 20:21:17 +0200

    converted switch.py to reflect arduino code instead of python in wemos

--- scripts/python/switch.py
+++ scripts/python/switch.py
@@ -1,30 +1,34 @@
 #!/bin/python
 
+# Some imports.
 import argparse
-#import requests
-import subprocess
+import requests
 
+
+# Argparse config and adding arguments.
 parser = argparse.ArgumentParser(description="Turn a switch on or off.")
 parser.add_argument("-A", "--all", help="All of them")
-#parser.add_argument("-o", "--overhead", help="Overhead lamp")
-#parser.add_argument("-s", "--standing", help="Standing lamp")
+# parser.add_argument("-o", "--overhead", help="Overhead lamp")
+# parser.add_argument("-s", "--standing", help="Standing lamp")
 parser.add_argument("-a", "--amp", help="Amplifier")
 args = parser.parse_args()
 
 
-def request(ip,power):
-	subprocess.call(["curl", "-sS", "http://192.168.1." + str(ip) + "/?power=" + str(power)])
+# A function to enter the ip address
+def ip(val):
+	return "http://192.168.1." + val + "/power"
 
 
+# Check which arg was passed and act accordingly.
 if args.all:
-	#request(221,args.all)
-	#request(222,args.all)
-	request(223,args.all)
+	# response = requests.post(ip("221"), data={"power": str(args.all)})
+	# response = requests.post(ip("222"), data={"power": str(args.all)})
+	response = requests.post(ip("223"), data={"power": str(args.all)})
 else:
-	#if args.overhead:
-		#request(221,args.overhead)
-	#if args.standing:
-		#request(222,args.standing)
+	# if args.overhead:
+	# 	response = requests.post(ip("221"), data={"power": str(args.overhead)})
+	# if args.standing:
+	# 	response = requests.post(ip("222"), data={"power": str(args.standing)})
 	if args.amp:
-		request(223,args.amp)
+		response = request.post(ip("223"), data={"power": str(args.amp)})
 
