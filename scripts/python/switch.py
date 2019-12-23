88c8bb0b4d23f2e54f969402675b543b0d23a79e
Alex de Wekker <alexdewekker6@gmail.com>
2019-12-23 22:54:13 +0100

    added the overhead wifiplug

--- scripts/python/switch.py
+++ scripts/python/switch.py
@@ -8,7 +8,7 @@ import requests
 # Argparse config and adding arguments.
 parser = argparse.ArgumentParser(description="Turn a switch on or off.")
 parser.add_argument("-A", "--all", help="All of them")
-# parser.add_argument("-o", "--overhead", help="Overhead lamp")
+parser.add_argument("-o", "--overhead", help="Overhead lamp")
 # parser.add_argument("-s", "--standing", help="Standing lamp")
 parser.add_argument("-a", "--amp", help="Amplifier")
 args = parser.parse_args()
@@ -21,12 +21,12 @@ def ip(val):
 
 # Check which arg was passed and act accordingly.
 if args.all:
-	# response = requests.post(ip("221"), data={"power": str(args.all)})
+	response = requests.post(ip("221"), data={"power": str(args.all)})
 	# response = requests.post(ip("222"), data={"power": str(args.all)})
 	response = requests.post(ip("223"), data={"power": str(args.all)})
 else:
-	# if args.overhead:
-	# 	response = requests.post(ip("221"), data={"power": str(args.overhead)})
+	if args.overhead:
+		response = requests.post(ip("221"), data={"power": str(args.overhead)})
 	# if args.standing:
 	# 	response = requests.post(ip("222"), data={"power": str(args.standing)})
 	if args.amp:
